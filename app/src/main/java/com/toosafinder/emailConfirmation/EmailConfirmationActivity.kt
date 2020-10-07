e734d9cc32dadbd6737243d56808b48077282357
S.Pavlov <s.pavlov1@g.nsu.ru>
2020-10-07 18:40:14 +0700

    Подправил часть комментариев пул реквеста #3993 @ 20 минут

---
+++ app/src/main/java/com/toosafinder/emailConfirmation/EmailConfirmationActivity.kt
@@ -0,0 +1,33 @@
+package com.toosafinder.emailConfirmation
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.toosafinder.R
+
+class EmailConfirmationActivity : AppCompatActivity() {
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        //вот тут нужен какой-то фон, надо узнать у пацанов
+        setContentView(R.layout.activity_login)
+
+        //val action: String? = intent?.action
+
+        //Надо придумать как обрабатывать
+        val data: Uri = intent?.data!!
+        val emailToken: String = parseData(data)
+
+    }
+
+//     Попытка переделать с Sealed class, но не понял зачем он тут, у нас же просто вытаскивается значение uuid,
+//     нет наших никаких наших состояний (мб я просто тупой и не понял)
+//    private fun parseData(uuidFromUri: UuidFromUrl): String = when (uuidFromUri) {
+//        is UuidFromUrl.ValidUuid -> uuidFromUri.data.lastPathSegment!!
+//        is UuidFromUrl.InvalidUuid -> getString(uuidFromUri.error)
+//    }
+    private fun parseData(data: Uri): String {
+            return data.lastPathSegment!!
+    }
+}
