20e3e4cb99e780a23cd4143d6e47bda93396956f
S.Pavlov <s.pavlov1@g.nsu.ru>
2020-10-10 07:26:34 +0700

    created package

--- app/src/main/java/com/toosafinder/restorePassword/RestorePasswordActivity.kt
+++ app/src/main/java/com/toosafinder/restorePassword/RestorePasswordActivity.kt
@@ -1,7 +1,14 @@
 package com.toosafinder.restorePassword
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class RestorePasswordActivity : AppCompatActivity(){
 
+    private lateinit var resorePasswordViewModel : RestorePasswordViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+
+    }
 }
