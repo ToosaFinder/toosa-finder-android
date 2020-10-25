703f5423899f176d064c69de3d95add560731686
S.Pavlov <s.pavlov1@g.nsu.ru>
2020-10-25 19:14:05 +0700

    small fix

--- app/src/main/java/com/toosafinder/emailConfirmation/EmailConfirmationDataSource.kt
+++ app/src/main/java/com/toosafinder/emailConfirmation/EmailConfirmationDataSource.kt
@@ -7,7 +7,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 import java.util.*
 
-private const val checkURL : String = "kakoi-to URL"
+private const val checkURL : String = "/user/emailConfirmed"
 interface EmailConfirmationDataSource {
 
     @GET(checkURL)
