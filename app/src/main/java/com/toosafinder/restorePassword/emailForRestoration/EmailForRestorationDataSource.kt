3bd32f10a619de7d4abdbf98c743141b714728d6
S.Pavlov <s.pavlov1@g.nsu.ru>
2020-11-08 19:40:11 +0700

    More fix

--- app/src/main/java/com/toosafinder/restorePassword/emailForRestoration/EmailForRestorationDataSource.kt
+++ app/src/main/java/com/toosafinder/restorePassword/emailForRestoration/EmailForRestorationDataSource.kt
@@ -5,11 +5,10 @@ import com.toosafinder.network.HTTPRes
 import retrofit2.http.Body
 import retrofit2.http.POST
 
-private const val restoreURL : String = "/user/resetPassword"
+private const val restoreURL : String = "/user/restore-password"
 
 interface EmailForRestorationDataSource {
 
     @POST(restoreURL)
-
     suspend fun restorePassword(@Body req: PasswordRestoreReq) : HTTPRes<Unit>
 }
