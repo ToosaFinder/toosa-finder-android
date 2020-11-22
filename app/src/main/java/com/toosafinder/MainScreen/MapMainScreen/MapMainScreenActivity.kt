5a5a5cd17dcc5624a3dc0b7459876c30865cbe22
S.Pavlov <s.pavlov1@g.nsu.ru>
2020-11-22 20:48:15 +0700

    Работа с google maps

--- app/src/main/java/com/toosafinder/MainScreen/MapMainScreen/MapMainScreenActivity.kt
+++ app/src/main/java/com/toosafinder/MainScreen/MapMainScreen/MapMainScreenActivity.kt
@@ -1,28 +1,44 @@
 package com.toosafinder.MainScreen.MapMainScreen
 
 import android.os.Bundle
+
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.toosafinder.R
 
-class MapMainScreenActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapMainScreenActivity :  AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Log.d("check", "check0")
         setContentView(R.layout.map_main_screen)
-
+        Log.d("check", "check1")
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-
+        Log.d("check", "check2")
         mapFragment.getMapAsync(this@MapMainScreenActivity)
+
+        Log.d("check", "check3")
 
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        TODO("Not yet implemented")
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        mMap.addMarker(MarkerOptions()
+            .position(sydney).title("Marker in Sydney")
+            .snippet("В Сиднее много котов"))
     }
 }
