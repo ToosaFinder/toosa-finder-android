78a170bde23170e7fa76c31fccfc6e642bf9acc3
Max <m.golikov@g.nsu.ru>
2020-11-26 17:09:15 +0700

    Some more bug fixes

---
+++ app/src/main/java/com/toosafinder/restorePassword/restorePassword/RestorePasswordAPI.kt
@@ -0,0 +1,15 @@
+package com.toosafinder.restorePassword.restorePassword
+
+import com.toosafinder.api.login.PasswordSetReq
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface RestorePasswordAPI {
+    companion object {
+        const val registerURL : String = "/user/set-password"
+    }
+
+    @POST(registerURL)
+    suspend fun registerPassword(@Body req : PasswordSetReq) : Response<Unit>
+}
--- app/src/main/java/com/toosafinder/restorePassword/restorePassword/RestorePasswordRepository.kt
+++ app/src/main/java/com/toosafinder/restorePassword/restorePassword/RestorePasswordRepository.kt
@@ -3,12 +3,10 @@ package com.toosafinder.restorePassword.restorePassword
 import com.toosafinder.api.login.PasswordSetReq
 import com.toosafinder.network.HTTPRes
 import com.toosafinder.network.convertAnswer
-import retrofit2.Response
 
 class RestorePasswordRepository(
     private val dataSource: RestorePasswordAPI
 ) {
-
 
     suspend fun registerPassword(emailToken : String, password : String) : HTTPRes<Unit>{
         val res = dataSource.registerPassword(PasswordSetReq(emailToken, password))
